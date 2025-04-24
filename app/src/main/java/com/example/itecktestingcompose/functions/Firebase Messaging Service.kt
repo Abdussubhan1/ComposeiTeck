fc0b437574addbf5d843dc1944299f504e1e9e23
Abdus Subhan <itssubhan_m@yahoo.com>
2025-04-24 17:15:47 +0500

    FCM messages service work

--- app/src/main/java/com/example/itecktestingcompose/functions/Firebase Messaging Service.kt
+++ app/src/main/java/com/example/itecktestingcompose/functions/Firebase Messaging Service.kt
@@ -1,14 +1,53 @@
 package com.example.itecktestingcompose.functions
 
+import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import com.google.firebase.messaging.FirebaseMessagingService
 import android.util.Log
+import androidx.core.app.NotificationCompat
+import com.example.itecktestingcompose.R
 import com.google.firebase.messaging.RemoteMessage
 
 class MyFirebaseMessagingService : FirebaseMessagingService() {
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
-        // Handle incoming FCM messages here
+
+        if (remoteMessage.data.isNotEmpty()) {
+
+            val title = remoteMessage.data["title"] ?: "Default Title"
+            val body = remoteMessage.data["body"] ?: "Default Body"
+
+            showNotification(title, body)
+
+        }
         Log.d("FCM Message", "Received message: ${remoteMessage.data}")
     }
+
+    @SuppressLint("ServiceCast")
+    private fun showNotification(title: String, message: String) {
+        val channelId = "my_channel_id"
+        val channelName = "My Notification Channel"
+
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+
+        val importance = NotificationManager.IMPORTANCE_HIGH
+        val channel = NotificationChannel(channelId, channelName, importance)
+        notificationManager.createNotificationChannel(channel)
+
+        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.ic_notification) // Make sure you have this icon in drawable
+            .setContentTitle(title)
+            .setContentText(message)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setAutoCancel(true)
+
+        notificationManager.notify(0, notificationBuilder.build())
+    }
+
 
     override fun onNewToken(token: String) {
         super.onNewToken(token)
