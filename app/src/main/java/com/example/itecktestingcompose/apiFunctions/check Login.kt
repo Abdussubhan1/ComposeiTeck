f710aeb0fbebf63eb06e0262441aa76f19a59a39
Abdus Subhan <itssubhan_m@yahoo.com>
2025-06-05 13:05:23 +0500

    modification

--- app/src/main/java/com/example/itecktestingcompose/apiFunctions/check Login.kt
+++ app/src/main/java/com/example/itecktestingcompose/apiFunctions/check Login.kt
@@ -6,8 +6,7 @@ import com.example.itecktestingcompose.objects.ServiceBuilder
 
 data class checkLoginResponse(
     var success: Boolean,
-    var message: String,
-    var code:Int
+    var message: String
 )
 
 
@@ -20,12 +19,12 @@ suspend fun checkLogin(
 
         if (response.isSuccessful && response.body() != null) {
             val responseBody = response.body()!!
-            checkLoginResponse(success = responseBody.success, message = responseBody.message,code = response.code())
+            checkLoginResponse(success = responseBody.success, message = responseBody.message)
         } else {
-            checkLoginResponse(success = false, message = "Response error: ${response.code()}",code = response.code())
+            checkLoginResponse(success = false, message = "Response error: ${response.code()}")
         }
     } catch (e: Exception) {
-        checkLoginResponse(success = false, message = "Error: ${e.localizedMessage ?: "Unknown error"}", code = 0)
+        checkLoginResponse(success = false, message = "Error: ${e.localizedMessage ?: "Unknown error"}")
     }
 }
 
