33abbf4068a372102c5936036b5b0b2acda52245
Abdus Subhan <itssubhan_m@yahoo.com>
2025-07-01 11:41:49 +0500

    changes

--- app/src/main/java/com/itecknologi/itecktestingcompose/objects/VehicleDetails.kt
+++ app/src/main/java/com/itecknologi/itecktestingcompose/objects/VehicleDetails.kt
@@ -1,12 +1,8 @@
 package com.itecknologi.itecktestingcompose.objects
 
-import com.itecknologi.itecktestingcompose.modelClasses.TrackerLocationItem
 import com.itecknologi.itecktestingcompose.modelClasses.VehData
 
 
 object vehicle_details {
     var dataList: List<VehData> = emptyList()
-}
-object deviceInstallationPlaces {
-    var places: ArrayList<TrackerLocationItem> = ArrayList()
 }
