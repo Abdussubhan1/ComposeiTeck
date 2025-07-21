6f9b74fd1c728abd43ee779f9e2747cbcc893ebc
Abdus Subhan <itssubhan_m@yahoo.com>
2025-07-21 17:23:34 +0500

    fixed issue

--- app/src/main/java/com/itecknologi/itecktestingcompose/apiFunctions/Vehicle Details.kt
+++ app/src/main/java/com/itecknologi/itecktestingcompose/apiFunctions/Vehicle Details.kt
@@ -11,23 +11,26 @@ data class getVehicleDetailsResponse(
     val message: String
 )
 
-suspend fun getVehicleDetails(T_ID:String,type:String): getVehicleDetailsResponse {
+suspend fun getVehicleDetails(T_ID: String, type: String): getVehicleDetailsResponse {
     return try {
         val response = ServiceBuilder.buildService(RetrofitInterface::class.java)
             .getVehicleDetails(T_ID, type)
 
-        if (response.isSuccessful) {
-            response.body()?.let { responseBody ->
-                if (responseBody.Success) {
-                    vehicle_details.dataList = responseBody.data
-                    getVehicleDetailsResponse(true, "Details Fetched")
-                }
+        response.body()?.let { responseBody ->
+            return if (responseBody.Success) { // correct lowercase "success"
+                vehicle_details.dataList = responseBody.data
+                getVehicleDetailsResponse(true, "Details Fetched")
+            } else {
+                getVehicleDetailsResponse(false, "Details Not Fetched")
             }
         }
+
+        // response.body() was null
         getVehicleDetailsResponse(false, "Vehicle Details Not Updated")
     } catch (e: Exception) {
         Log.d("cnicV", "Exception: ${e.localizedMessage ?: e}")
         getVehicleDetailsResponse(false, "unknown error")
     }
 }
+
 
