f626326c6d74998dcddb8052a84a5a9fe8123b81
Abdus Subhan <itssubhan_m@yahoo.com>
2025-07-23 16:29:54 +0500

    imports del

--- app/src/main/java/com/itecknologi/itecktestingcompose/appScreens/Notification Screen.kt
+++ app/src/main/java/com/itecknologi/itecktestingcompose/appScreens/Notification Screen.kt
@@ -1,10 +1,8 @@
 package com.itecknologi.itecktestingcompose.appScreens
 
-import androidx.annotation.DrawableRes
-import androidx.compose.foundation.BorderStroke
+
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -15,7 +13,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
@@ -27,9 +24,7 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -42,15 +37,10 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -215,24 +205,4 @@ fun NotificationCard(item: NotificationHistoryItem) {
     }
 }
 
-@Preview
-@Composable
-fun NotificationCardPreview() {
-    NotificationCard(
-        item = NotificationHistoryItem(
-            "You Have a New Installation",
-            "07-05-2025 13:88:00",
-            "New Installation"
-        )
-    )
-}
-
-@Preview
-@Composable
-fun NotificationScreen() {
-    NotificationScreen(
-        navController = NavHostController(LocalContext.current),
-        prefs = PreferenceManager(LocalContext.current)
-    )
-}
 
